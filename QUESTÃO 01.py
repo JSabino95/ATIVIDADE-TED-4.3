96deee39f914eef3c9d20d7cd88b1da62cdf13ec
Joallysson Sabino Alves da Silva <jjjoallysson@gmail.com>
2023-03-26 18:10:48 -0300

    Atividade

---
+++ QUESTÃO 01.py
@@ -0,0 +1,7 @@
+valor = float(input("digite valor: "))
+
+if valor < 0:
+    print(f'{valor} valor negativo')
+
+elif valor >= 0:
+    print(f'{valor} valor positivo')
---
+++ QUESTÃO 02.py
@@ -0,0 +1,6 @@
+quantidade_de_maçãs = int(input("digite quantidade: "))
+
+if quantidade_de_maçãs < 12:
+    print(quantidade_de_maçãs * 1.3)
+else:
+    print(quantidade_de_maçãs * 1)
